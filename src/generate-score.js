9ad1dd50dc9aad9c16c95dff71ba81acc4a01750
Rudy Hentzen <rudy.hentzen@ft.com>
2024-11-05 21:35:05 +0000

    feat: handle file arg

--- src/generate-score.js
+++ src/generate-score.js
@@ -1,4 +1,11 @@
 const generateScore = () => {
+  const filename = process.argv[2];
+  console.log("filename", filename);
+  if (!filename) {
+    console.error("Please provide a filename as an argument");
+    process.exit(1);
+  }
+
   console.log("Test score");
 };
 
--- tests/generate-score.test.js
+++ tests/generate-score.test.js
@@ -1,19 +1,54 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  afterAll,
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
 import generateScore from "../src/generate-score.js";
 
 const logSpy = vi.spyOn(console, "log");
 
-describe("when a valid file is provided", () => {
+describe("when generating a score", () => {
+  const originalArgv = process.argv;
+
   beforeEach(() => {
     logSpy.mockClear();
+    process.argv = ["node", "index.js", "mock-answer-file.json"];
   });
 
   afterEach(() => {
+    process.argv = originalArgv;
+  });
+
+  afterAll(() => {
     vi.restoreAllMocks();
   });
 
-  it("produces a score", () => {
-    generateScore();
-    expect(logSpy).toHaveBeenCalledWith("Test score");
+  describe("and a valid file is provided", () => {
+    it("produces a score", () => {
+      generateScore();
+      expect(logSpy).toHaveBeenCalledWith("Test score");
+    });
+  });
+
+  describe("and a file is not provided", () => {
+    const originalExit = process.exit;
+
+    beforeEach(() => {
+      process.argv = ["node", "index.js"];
+      process.exit = vi.fn();
+    });
+
+    afterEach(() => {
+      process.exit = originalExit;
+    });
+
+    it('logs filename not provided"', () => {
+      generateScore();
+      expect(logSpy).toHaveBeenCalledWith("filename", undefined);
+    });
   });
 });
