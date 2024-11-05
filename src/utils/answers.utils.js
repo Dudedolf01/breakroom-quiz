d04c35d3bfa5701712b4fc76463356b344ae35c0
Rudy Hentzen <rudy.hentzen@ft.com>
2024-11-05 22:50:31 +0000

    feat: refactor answer logic

--- src/utils/answers.utils.js
+++ src/utils/answers.utils.js
@@ -1,19 +1,21 @@
 const answersConfig = {
-  enjoys_job: (answer) => parseYesNo(answer),
-  respected_by_managers: (answer) => parseYesNo(answer),
-  good_for_carers: (answer) => parseYesNo(answer), // should this be carers or careers?
-  unpaid_extra_work: (answer) => parseYesNo(answer),
+  enjoys_job: (answers) => parseYesNo(answers, "enjoys_job"),
+  respected_by_managers: (answers) =>
+    parseYesNo(answers, "respected_by_managers"),
+  good_for_carers: (answers) => parseYesNo(answers, "good_for_carers"), // should this be carers or careers?
+  unpaid_extra_work: (answers) => parseYesNo(answers, "unpaid_extra_work"),
 };
 
-const parseYesNo = (answer) => {
+const parseYesNo = (answers, answerKey) => {
+  const answer = answers[answerKey];
   return answer === "yes" ? 1 : 0;
 };
 
 export const parseAnswersAndReturnScore = (answers) => {
-  return Object.keys(answers).reduce((acc, key) => {
+  return Object.keys(answersConfig).reduce((acc, key) => {
     const answerConfig = answersConfig[key];
     if (answerConfig) {
-      acc += answerConfig(answers[key]);
+      acc += answerConfig(answers);
     }
     return acc;
   }, 0);
